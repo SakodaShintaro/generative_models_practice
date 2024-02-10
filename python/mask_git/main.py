5bfb33e0f17893ddd894e74106227745b31fb088
Shintaro Sakoda <sakoda@keio.jp>
2024-02-10 09:58:18 +0900

    Fixed train code
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/mask_git/main.py
+++ python/mask_git/main.py
@@ -9,19 +9,15 @@ from flax.training import train_state
 from data_loader import DataLoader
 import os
 import numpy as np
+from tqdm import tqdm
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("data_dir", type=str)
     return parser.parse_args()
-
-
-def load_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img / 255.0
-    return img
 
 
 def loss_fn(params, batch):
@@ -64,16 +60,19 @@ if __name__ == "__main__":
         tx=optimizer,
     )
 
-    num_epochs = 10
+    num_epochs = 20
 
     save_dir = "./result_test/"
 
     for epoch in range(num_epochs):
         train_loader.shuffle()
-        for batch in train_loader:
+        loss_sum = 0
+        loss_num = 0
+        for batch in tqdm(train_loader):
             state, loss = train_step(state, batch)
-            print(loss)
-        print(f'Epoch {epoch}, Loss: {loss}')
+            loss_sum += batch.shape[0] * loss
+            loss_num += batch.shape[0]
+        print(f'Epoch {epoch}, Loss: {loss_sum / loss_num:.3f}')
 
         curr_save_dir = f"{save_dir}/{epoch:04d}"
         os.makedirs(curr_save_dir, exist_ok=True)
