1436d535a4e91a9971cb876e883b4e865b99bb99
sakoda <sakoda@keio.jp>
2025-04-12 00:08:52 +0000

    Fixed to run
    Signed-off-by: sakoda <sakoda@keio.jp>

--- python/RWKV-7/main.py
+++ python/RWKV-7/main.py
@@ -9,6 +9,26 @@ def f(S, w, z, b, v, k):
     return S
 
 
+def f_impl(S, sensitivity_mats, w, z, b, v, k):
+    S = S * w.mT - S @ z * b.mT + v * k.mT
+    return (S, sensitivity_mats)
+
+
+@jax.custom_vjp
+def custum_f(S, sensitivity_mats, z, w, b, v, k):
+    return f_impl(S, sensitivity_mats, w, z, b, v, k)
+
+
+def custum_fwd(S, sensitivity_mats, w, z, b, v, k):
+    f_out, vjp_func = jax.vjp(f_impl, S, sensitivity_mats, w, z, b, v, k)
+    return f_out, (vjp_func, f_out)
+
+
+def custum_bwd(res, g):
+    vjp_func, f_out = res
+    return vjp_func(g)
+
+
 def compute_loss(params, init_S):
     w, z, b, v, k = params
     curr_S = init_S
@@ -19,9 +39,14 @@ def compute_loss(params, init_S):
     return jnp.mean(curr_S)
 
 
+def compute_loss2(curr_S, sensitivity_mats, w, z, b, v, k):
+    curr_S, sensitivity_mats = custum_f(curr_S, sensitivity_mats, w, z, b, v, k)
+    return jnp.mean(curr_S)
+
+
 if __name__ == "__main__":
-    HEAD_NUM = 16
-    HEAD_SIZE = 64
+    HEAD_NUM = 2
+    HEAD_SIZE = 8
     TIMESTEP = 10
 
     rng = jax.random.PRNGKey(0)
@@ -43,3 +68,19 @@ if __name__ == "__main__":
     print(f"b: {grad_b.shape=}")
     print(f"v: {grad_v.shape=}")
     print(f"k: {grad_k.shape=}")
+
+    custum_f.defvjp(custum_fwd, custum_bwd)
+
+    sensitivity_w = jax.numpy.zeros((HEAD_NUM, HEAD_SIZE, HEAD_SIZE, HEAD_SIZE))
+    sensitivity_z = jax.numpy.zeros((HEAD_NUM, HEAD_SIZE, HEAD_SIZE, HEAD_SIZE))
+    sensitivity_b = jax.numpy.zeros((HEAD_NUM, HEAD_SIZE, HEAD_SIZE, HEAD_SIZE))
+    sensitivity_v = jax.numpy.zeros((HEAD_NUM, HEAD_SIZE, HEAD_SIZE, HEAD_SIZE))
+    sensitivity_k = jax.numpy.zeros((HEAD_NUM, HEAD_SIZE, HEAD_SIZE, HEAD_SIZE))
+    sensitivity_mats = (sensitivity_w, sensitivity_z, sensitivity_b, sensitivity_v, sensitivity_k)
+    carry = (curr_S, sensitivity_mats)
+
+    for i in range(w.shape[0]):
+        print(f"{i=}")
+        custum_f(curr_S, sensitivity_mats, w[i], z[i], b[i], v[i], k[i])
+        grad = jax.grad(compute_loss2)(curr_S, sensitivity_mats, w[i], z[i], b[i], v[i], k[i])
+        exit(0)
