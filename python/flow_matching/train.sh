3dd35ed23df9edd7a818f01c195b4c061b6d13ce
Shintaro Sakoda <sakoda@keio.jp>
2026-05-26 08:51:31 +0900

    Fixed args
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/flow_matching/train.sh
+++ python/flow_matching/train.sh
@@ -4,8 +4,5 @@ set -eux
 cd $(dirname $0)
 
 uv run python train_flow_matching.py \
-    --epochs=3000 \
-    --batch_size=500 \
-    --learning_rate=1e-4 \
     --data_path="./data/" \
     --results_dir="./results/$(date '+%Y%m%d_%H%M%S')/"
